069e4f42273ffaaa2b87fcb368943b3fcb81af82
Rodrigo Prado de Jesus <royopa@gmail.com>
2020-10-26 22:17:14 -0300

    ajustado aula propaganda

--- Propaganda/app/src/main/java/com/example/propaganda/MenuActivity.java
+++ Propaganda/app/src/main/java/com/example/propaganda/MenuActivity.java
@@ -2,7 +2,10 @@ package com.example.propaganda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        //Ligação do componente da classe com os componentes da interface.
+        Button btnSobre = (Button) findViewById(R.id.btnSobre);
+
+        btnSobre.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Botão sobre clicado");
+                Intent activity2Intent;
+                activity2Intent = new Intent(getApplicationContext(), SobreActivity.class);
+                startActivity(activity2Intent);
+            }
+        });
+
+        Button btnTelefone = (Button) findViewById(R.id.btnTelefone);
+        btnTelefone.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Botão telefone clicado");
+                Intent activity2Intent;
+                activity2Intent = new Intent(getApplicationContext(), TelefoneActivity.class);
+                startActivity(activity2Intent);
+            }
+        });
     }
 }
--- Propaganda/app/src/main/java/com/example/propaganda/SobreActivity.java
+++ Propaganda/app/src/main/java/com/example/propaganda/SobreActivity.java
@@ -2,7 +2,10 @@ package com.example.propaganda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SobreActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class SobreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
+
+        //Ligação do componente da classe com os componentes da interface.
+        Button btnVoltarMenu = (Button) findViewById(R.id.btnVoltarMenu);
+
+        btnVoltarMenu.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Botão voltar clicado");
+                Intent activity2Intent;
+                activity2Intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(activity2Intent);
+            }
+        });
     }
 }
